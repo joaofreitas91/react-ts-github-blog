7ad1a4907ce2fc6f49dca3f7b460b329b29752aa
Joao Paulo Freitas <jp.contatoprofissional@gmail.com>
2023-08-31 01:02:14 -0300

    refactor: useGithubUser

--- src/services/hooks/useGithubUser.ts
+++ src/services/hooks/useGithubUser.ts
@@ -11,26 +11,24 @@ interface GithubUser {
   followers: number
 }
 
-export function useGithubUser() {
-  return useQuery<GithubUser>(
-    'user',
-    async () => {
-      const { data } = await api.get(
-        `/users/${import.meta.env.VITE_GITHUB_USER}`,
-      )
-
-      return {
-        name: data.name,
-        company: data.company,
-        bio: data.bio,
-        blog: data.blog,
-        login: data.login,
-        avatar_url: data.avatar_url,
-        followers: data.followers,
-      }
-    },
-    {
-      staleTime: Infinity,
-    },
+export async function getGithubUser(): Promise<GithubUser> {
+  const { data } = await api.get<GithubUser>(
+    `/users/${import.meta.env.VITE_GITHUB_USER}`,
   )
+
+  return {
+    name: data.name,
+    company: data.company,
+    bio: data.bio,
+    blog: data.blog,
+    login: data.login,
+    avatar_url: data.avatar_url,
+    followers: data.followers,
+  }
+}
+
+export function useGithubUser() {
+  return useQuery('user', getGithubUser, {
+    staleTime: Infinity,
+  })
 }
